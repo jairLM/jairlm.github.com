49776d567e7bc8cf1cdebf975a3a15f94e779bd5
Luis Jair Lopez Murillo <brook_98@live.com>
2023-12-14 16:10:24 -0600

    mostreo de datos en el html

--- boletines/javascript/api.js
+++ boletines/javascript/api.js
@@ -8,6 +8,12 @@ async function getData(){
     try {
         
         const responseJson = await fetch(url); //se obtienen datos de la api
+        if(!responseJson.ok){
+
+            throw new Error(`Error en la solicitud: ${responseJson.status}`)
+
+        }
+
         const resolve = await responseJson.json();
         return resolve
         
@@ -16,18 +22,45 @@ async function getData(){
     } catch (error) {
         console.error(error.message);
     }
-
-
-
-
 }
+
+
 
 async function fetchData(){
-    const series = await getData();
+    const response = await getData();
+    if (response.metadata[0].Code == "00"){
+        console.log("todo bien");
+        printSerie(response);
+    }
 
-    const kosas = series.metadata.map(serie=>{return serie })
 
-    console.log(series, kosas)
+    console.log(response.serieResponse.serie)
 }
-
 fetchData();
+
+
+
+
+
+
+async function printSerie(response){
+    const series =  response.serieResponse.serie.map(serie =>serie);
+    const print = series.map( serie =>{
+        const starIcons = Array.from({ length: serie.rating }, () => '<i class="fa-solid fa-star"></i>').join('');
+
+        return `
+                <tr>
+                    <td>${serie.id}</td>
+                    <td>${serie.name}</td>
+                    <td>${serie.releaseYear}</td>
+                    <td>${starIcons}</td>
+                    <td><a href="./series/${serie.name}.html" target="_self">Ver más</a></td>
+                </tr>       
+        `
+    }).join("");
+
+    const idDom = document.getElementById('body');
+    idDom.innerHTML=print;
+
+
+}
